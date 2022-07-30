abb64660fe256e0249eff9a4ee0fbb5a5b3d7586
Elijah <elijah.ivanik@inbox.ru>
2022-07-30 09:36:35 +0500

    feat: prettier+eslint

--- src/js/modules/index.tsx
+++ src/js/modules/index.tsx
@@ -1,14 +1,15 @@
-import React from "react"
-import css from "../../scss/app.module.scss"
+import React from "react";
+import css from "../../scss/app.module.scss";
 
 const App = () => {
   return (
     <div className={css.content}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit maiores omnis voluptatum alias rerum! Quia
-      itaque quis consequatur tempora neque asperiores iusto officia quas cumque exercitationem? Ab reiciendis tempore
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit maiores
+      omnis voluptatum alias rerum! Quia itaque quis consequatur tempora neque
+      asperiores iusto officia quas cumque exercitationem? Ab reiciendis tempore
       fugiat
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
