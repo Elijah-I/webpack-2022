97836535944c41c0f52c2c795431d127201dd5d7
Elijah <elijah.ivanik@inbox.ru>
2022-07-30 21:54:39 +0500

    fix: remove hot reload for prod

--- webpack/index.js
+++ webpack/index.js
@@ -1,4 +1,5 @@
 import { Path } from "./service/path.js"
+import { isDev } from "./service/env.js"
 import { filename } from "./service/filename.js"
 
 const BaseConfig = {
@@ -7,9 +8,7 @@ const BaseConfig = {
   mode: process.env.NODE_ENV,
 
   entry: {
-    app: Path("src/js/app.tsx"),
-    hot: "webpack/hot/dev-server.js",
-    client: "webpack-dev-server/client/index.js?hot=true&live-reload=true"
+    app: "./index.tsx"
   },
 
   output: {
@@ -22,6 +21,12 @@ const BaseConfig = {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"],
     modules: [Path("src"), Path("node_modules")]
   }
+}
+
+if (isDev) {
+  BaseConfig.hot = "webpack/hot/dev-server.js"
+  BaseConfig.client =
+    "webpack-dev-server/client/index.js?hot=true&live-reload=true"
 }
 
 export default BaseConfig
