dc7798298e4e78f32cf064413d2f516c1617e1ac
Elijah <elijah.ivanik@inbox.ru>
2023-01-18 12:09:58 +0300

    refactor: change favicon model to Light

--- webpack/plugins/favicon.js
+++ webpack/plugins/favicon.js
@@ -1,16 +1,15 @@
-import { isDev } from "../service/env.js"
-import { Path } from "../service/path.js"
-import FaviconsWebpackPlugin from "favicons-webpack-plugin"
+import { isDev } from "../service/env.js";
+import { Path } from "../service/path.js";
+import FaviconsWebpackPlugin from "favicons-webpack-plugin";
 
-const logo = Path("src/assets/favicon/favicon.svg")
+const logo = Path("src/assets/favicon/favicon.svg");
 
 const settings = isDev
   ? { logo, prefix: "" }
   : {
       logo,
       inject: true,
-      mode: "webapp",
-      devMode: "webapp",
+      mode: "light",
       prefix: "assets/favicon/",
       outputPath: Path("build/assets/favicon/"),
 
@@ -18,6 +17,6 @@ const settings = isDev
         background: "#ddd",
         theme_color: "#333"
       }
-    }
+    };
 
-export const favicon = new FaviconsWebpackPlugin(settings)
+export const favicon = new FaviconsWebpackPlugin(settings);
