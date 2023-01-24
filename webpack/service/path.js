e99cf6dc9aeb0520e186fd9475d2caa464f2f838
Elijah <elijah.ivanik@inbox.ru>
2023-01-24 19:50:34 +0300

    fix: linux path

--- webpack/service/path.js
+++ webpack/service/path.js
@@ -1,7 +1,8 @@
-import { fileURLToPath } from "url"
-import path from "path"
+import { fileURLToPath } from "url";
+import path from "path";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const Path = (src) => path.dirname(path.dirname(__dirname, "webpack")) + `\\${src}`
+export const Path = (src) =>
+  path.join(path.dirname(path.dirname(__dirname, "webpack")), src);
